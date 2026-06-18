d9aeef8c4ae3b1591619f60b80d172c2e2ba73a8
Hmitsev <hmitsev@intercars.eu>
2026-06-18 22:25:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -513,16 +513,22 @@ if uploaded_files and len(uploaded_files) > 0:
         # ======================================================
         if source_type == "PDF":
 
-            reader = PdfReader(file)
-            text = ""
+            if menu == "NESTE":
 
-            for page in reader.pages:
-                text += page.extract_text() + "\n"
+    df = parse_neste_excel(file)
 
-            if menu == "Castrol":
-                df = parse_castrol(text)
-            else:
-                df = parse_motul(text)
+elif source_type == "PDF":
+
+    reader = PdfReader(file)
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text() + "\n"
+
+    if menu == "Castrol":
+        df = parse_castrol(text)
+    else:
+        df = parse_motul(text)
 
         # ======================================================
         # ✅ EXCEL (FIXED)
