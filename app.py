2d34aee8744f661a13be338094f2005e129dae45
Hmitsev <hmitsev@intercars.eu>
2026-06-18 21:49:40 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -455,6 +455,38 @@ def parse_motul(text):
                 liters_per_unit = 0
                 units_in_box = 1
 
+           return pd.DataFrame(rows)
+        # ======================================================
+        # ✅ КОД
+        # ======================================================
+        if "HS code" in line:
+            code = re.search(r"HS code\s*:\s*(\d+)", line)
+
+            if code:
+                code_value = code.group(1)[:8]
+
+                if current_qty * units_in_box * liters_per_unit > 100000:
+                    real_qty = current_qty
+                else:
+                    if units_in_box > 1 and liters_per_unit <= 5:
+                        real_qty = current_qty * units_in_box
+                    else:
+                        real_qty = current_qty
+
+                rows.append({
+                    "Тарифен код": code_value,
+                    "Количество": real_qty,
+                    "wid": liters_per_unit,
+                    "kolichestvo": real_qty * liters_per_unit,
+                    "тегло": current_weight
+                })
+
+                # ✅ RESET
+                current_qty = 0
+                current_weight = 0
+                liters_per_unit = 0
+                units_in_box = 1
+
     return pd.DataFrame(rows)
 
 
