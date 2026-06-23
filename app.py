cac81ea045eb8b02c498d3a009fe9aa2cfa33544
Hmitsev <hmitsev@intercars.eu>
2026-06-24 01:19:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ button[data-testid="baseButton-secondary"] p {
 
 
 # ✅ SIDEBAR
-menu = st.sidebar.selectbox("Suppliers", ["CASTROL", "MOTUL", "NESTE", "FLUKAR", "RAVENOL", "VALVOLINE", "ORLEN", "Chempioil (FANFARO)", "FUCHS", "FEBI", "Gasoline","NISTA", "АВИАТРАНС MA-FRA", "AMTRA" , "AUTO MEGA" ,"EMINIA"])
+menu = st.sidebar.selectbox("Suppliers", ["CASTROL", "MOTUL", "NESTE", "FLUKAR", "GASOLIN", "VALVOLINE", "ORLEN", "Chempioil (FANFARO)", "FUCHS", "FEBI", ""ELROMI RONAX"","NISTA", "AMTRA" , "AUTO MEGA" ,"EMINIA" ,"Brehman"])
 
 # ✅ RESET при смяна на supplier
 if "prev_supplier" not in st.session_state:
