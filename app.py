61741ecc4b613e26982c63eaef703dcc1375c915
Hmitsev <hmitsev@intercars.eu>
2026-07-09 18:25:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1097,6 +1097,12 @@ def parse_fuchs(text):
 # ======================================================
 def parse_gasoline(text):
 
+    st.write("✅ GASOLINE PARSER")
+
+    st.text(text[:5000])
+
+    rows = []
+
     rows = []
 
     pattern = re.compile(
